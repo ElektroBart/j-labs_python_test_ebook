20bea5bd2be1028b0122bba798dc0a0ebee1db98
Bartek <bartlomiej.juszczak@j-labs.pl>
2024-01-26 11:28:53 +0100

    added login_page fixture, refactored test_login_page.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from src.pages.login_page import LoginPage
 
 
 @pytest.fixture(scope="class")
@@ -7,3 +8,10 @@ def browser(request):
     driver = webdriver.Chrome()
     yield driver
     driver.quit()
+
+
+@pytest.fixture
+def login_page(browser):
+    log_page = LoginPage(browser)
+    log_page.open_login_page()
+    return log_page
--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,35 +1,24 @@
-from src.pages.login_page import LoginPage
-
-
 class TestLoginPage:
     correct_username = "standard_user"
     correct_password = "secret_sauce"
 
-    def test_successful_login(self, browser):
-        login_page = LoginPage(browser)
-        login_page.open_login_page()
+    def test_successful_login(self, login_page):
         products_page = login_page.login(self.correct_username, self.correct_password)
-        products_page.is_products_page_loaded()
+        assert products_page.is_products_page_loaded(), "Page isn't loaded"
         assert products_page.get_products_title() == "Products", "Page title doesn't match"
 
-    def test_empty_username_and_password(self, browser):
-        login_page = LoginPage(browser)
-        login_page.open_login_page()
+    def test_empty_username_and_password(self, login_page):
         login_page.click_login_button()
         assert login_page.get_error_message_text() == "Username and password are required", \
             "The error message doesn't match."
 
-    def test_empty_username(self, browser):
-        login_page = LoginPage(browser)
-        login_page.open_login_page()
+    def test_empty_username(self, login_page):
         login_page.enter_password(self.correct_password)
         login_page.click_login_button()
         assert login_page.get_error_message_text() == "Username is required", \
             "The error message doesn't match."
 
-    def test_empty_password(self, browser):
-        login_page = LoginPage(browser)
-        login_page.open_login_page()
+    def test_empty_password(self, login_page):
         login_page.enter_username(self.correct_username)
         login_page.click_login_button()
         assert login_page.get_error_message_text() == "Password is required", \
