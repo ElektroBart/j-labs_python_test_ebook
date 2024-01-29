81963afd55b174c87e163b4e53d62c949eb3df1f
Bartek <bartlomiej.juszczak@j-labs.pl>
2024-01-29 14:07:21 +0100

    added cross-browser functionality

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,25 @@ from selenium import webdriver
 from src.pages.login_page import LoginPage
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope='class')
 def browser(request):
-    driver = webdriver.Chrome()
+    browser = request.config.getoption("--browser")
+    print(browser)
+    if browser == "chrome":
+        driver = webdriver.Chrome()
+    elif browser == "firefox":
+        driver = webdriver.Firefox()
+    elif browser == "edge":
+        driver = webdriver.Edge()
+    else:
+        raise ValueError(f"Unsupported browser: {browser}")
+    request.cls.driver = driver
     yield driver
     driver.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome", help="Browser name: chrome, firefor or edge")
 
 
 @pytest.fixture
